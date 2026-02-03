c0042a21201690a97a8fc46eace386a41272769f
Anna Lyapisova <annalyapisova@yandex.ru>
2026-02-03 14:03:01 +0300

    Update test_pokemon.py

--- projects/tests/test_pokemon.py
+++ projects/tests/test_pokemon.py
@@ -3,7 +3,7 @@ import requests
 import pytest
 
 URL = 'https://api.pokemonbattle.ru/v2'
-TOKEN = 'ad9cd1291ad684d21d1dec2302de7905'
+TOKEN = 'USER_TOKEN'
 HEADER = {'Content-Type': 'application/json', 'trainer_token': TOKEN}
 TRAINER_ID = "46697"
 
@@ -14,4 +14,8 @@ def test_status_code ():
 def test_part_of_respons ():
     response_get = requests.get(url=f'{URL}/trainers', params={'trainer_id': TRAINER_ID})
     print(response_get.text)
-    assert response_get.json()['data'][0]['trainer_name']=='Nezuko'
+    
+@pytest.mark.parametrize('key, value', [('trainer_name','Nezuko'),('id',f'{TRAINER_ID}')])
+def test_parametrize(key,value):
+    response_parametrize = requests.get(url= f'{URL}/trainers',params = {'trainer_id': TRAINER_ID})
+    assert response_parametrize.json()["data"][0][key] == value'
